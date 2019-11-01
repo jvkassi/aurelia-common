90aa2d97638f62b15622c4f4d52578ba4251fee2
Jean-Vincent KASSI <jekas@smile.ci>
2019-11-01 15:53:43 +0000

    chore(release): 0.0.10

--- dist/esnext/services/dialog.d.ts
+++ dist/esnext/services/dialog.d.ts
@@ -1,10 +1,5 @@
 export declare class Dialog {
     private dialog;
-    confirm(title?: string, text?: string): Promise<{}>;
-    prompt({ label, title, type, text }: {
-        label: string;
-        title?: string;
-        type?: string;
-        text?: string;
-    }): Promise<{}>;
+    confirm(title?: string, text?: string): Promise<any>;
+    prompt(label: string, title?: string, type?: string, text?: string): Promise<any>;
 }
--- dist/native-modules/aurelia-common.d.ts
+++ dist/native-modules/aurelia-common.d.ts
@@ -1,4 +1,2 @@
-import { Dialog } from './services/dialog';
 import { FrameworkConfiguration } from "aurelia-framework";
 export declare function configure(config: FrameworkConfiguration): void;
-export { Dialog };
